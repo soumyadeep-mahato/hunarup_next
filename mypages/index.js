010a6302b72839b5adf4b30cf21589896c652474
Souhardya Das <66139595+dassouhardya@users.noreply.github.com>
2021-04-27 15:35:12 +0530

    Add files via upload

---
+++ mypages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Footer from '../comps/Footer'
+import Navbar from '../comps/Navbar'
+import styles from '../styles/Home.module.css'
+
+
+export default function Home() {
+   return(
+     <div>
+      
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}> Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+         when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        
+     </div>
+   )
+   }
